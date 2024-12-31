42af52809178d7fb75292e31c05a39e18a59b045
Nabiha Zaman <nabihazaman26@gmail.com>
2024-12-31 16:58:05 -0600

    created monkey model and set main page layout to display information about different monkey objects

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -1,7 +1,19 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Runtime.ConstrainedExecution;
+using System.Text.Json.Serialization;
 
 namespace MonkeyFinder.Model;
 
 public class Monkey
 {
+    //json2csharp website
+    //edit -> paste special -> json as classes 
+    //be careful about types and double check to make sure they are correct when using automated conversions
+    //error w method 2 - float instead of double
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string Image { get; set; }
+    public int Population { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
